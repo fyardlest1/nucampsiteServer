15279ad85d89ca3c14ddf1f26c18f7ed11277baf
Fyardlest <fyardlest1@gmail.com>
2020-08-17 21:03:46 -0400

    Express Session Part 2

--- app.js
+++ app.js
@@ -47,35 +47,36 @@ app.use(
   })
 );
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 // Basic Authentication
 function auth(req, res, next) {
   console.log(req.session);
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === "admin" && pass === "password") {
-      req.session.user = 'admin';
-      // res.cookie('user', 'admin', {signed: true});
-      return next(); // authorized
-    } else {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+    // const authHeader = req.headers.authorization;
+    // if (!authHeader) {
+    const err = new Error("You are not authenticated!");
+    // res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+    // const auth = Buffer.from(authHeader.split(" ")[1], "base64")
+    //   .toString()
+    //   .split(":");
+    // const user = auth[0];
+    // const pass = auth[1];
+    // if (user === "admin" && pass === "password") {
+    //   req.session.user = 'admin';
+    //   // res.cookie('user', 'admin', {signed: true});
+    //   return next(); // authorized
+    // } else {
+    //   const err = new Error("You are not authenticated!");
+    //   res.setHeader("WWW-Authenticate", "Basic");
+    //   err.status = 401;
+    //   return next(err);
+    // }
   } else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       return next();
     } else {
       const err = new Error('You are not authenticated!');
@@ -89,8 +90,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/partners', partnerRouter);
 app.use('/promotions', promotionRouter);
